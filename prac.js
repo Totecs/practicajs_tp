7208fda6b83436bc831f76d14e52cccc9cf9a89d
Totecs <tomasdelgadillo1602@gmail.com>
2026-06-10 15:07:25 -0300

    p8

--- prac.js
+++ prac.js
@@ -1,12 +1,17 @@
 let nombreEstudiante="Tomas Delgadillo";
 //
-const diaSemana=7;
+const diaSemana = 7;
 //
-let temperaturaActual=30.5;
+let temperaturaActual = 30.5;
 //
-let userConnect=true;
+let userConnect = true;
 //
-let nota1=6, nota2=4, nota3=8;
+let nota1 = 6, nota2 = 4, nota3 = 8;
 //
 let listaAlumnos = "Juan, Pedro, Ana";
+//
+let materiaFavorita = "Fisica";
+//
+let cantidadEntradas = 0;
+cantidadEntradas = 1;
 //
